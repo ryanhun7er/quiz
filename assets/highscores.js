5df2c35cc25ab8171fca03187076ce2417f796cb
Ryan Hunter <ryanhwilkinson@icloud.com>
2020-03-02 15:22:26 -0600

    updates to highscore js

--- assets/highscores.js
+++ assets/highscores.js
@@ -1,18 +1,19 @@
-const highScoresList = document.getElementById("highScoresList");
+//const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores"));
+const ul = document.querySelector("ul");
+
 
 console.log(highScores);
 
-var scoreDiv = document.getElementById("highScoresList");
+const scoreSubmit = score => {
+    const li = document.createElement('li')
+    li.textContent = score
+    ul.appendChild(li)
 
-highScores.forEach(function(score) {
+    console.log(scoreSubmit);
+}
 
-    var newScoreL = document.createElement("li");
 
-    newScoreL.textContent = score;
 
-    scoreDiv.appendChild(newScoreL);
 
-    console.log(scoreDiv);
-});
-
+//document.getElementById("highScoresList").innerHTML = highScores.player + ":" + highScores.score;
