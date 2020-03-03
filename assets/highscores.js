de64cfb9469fa6ef8b3590bbf8d3828ec3f8647b
Ryan Hunter <ryanhwilkinson@icloud.com>
2020-03-02 23:53:55 -0600

    highscore js finally working!

--- assets/highscores.js
+++ assets/highscores.js
@@ -1,23 +1,33 @@
-const highScoresList = document.getElementById("highScoresList");
-const highScores = JSON.parse(localStorage.getItem("highScores"));
+
+var highScoresList = document.querySelector("#highScoresList");
+var highScores = JSON.parse(localStorage.getItem("highScores"));
 
 console.log(highScores);
-console.log(highScoresList);
 
-var text ="";
-var i;
+
+renderScores();
+
+function renderScores() {
+
+highScoresList.innerHTML="";
 
 for (var i =0; i < highScores.length; i++) {
-    text += "Player:" + highScores[i].player.value;
-    text += "Score:" + highScores[i].score.value + "<br>";
+    var hs = highScores[i].score;
+    var pn = highScores[i].player;
+
+    var li = document.createElement("li");
+    li.textContent = pn + "~" + "     " + "     " + hs;
+    highScoresList.appendChild(li);
+    
+    }
 }
 
-document.getElementById("highScoresList").innerHTML = highScores;
-//const scoreSubmit = score => {
-   // const li = document.createElement('li')
-   // li.textContent = score
-  //  ul.appendChild(li)
+//when form is submitted
 
-  //  console.log(scoreSubmit);
-//}
+scoreForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+})
+
+
+
 
